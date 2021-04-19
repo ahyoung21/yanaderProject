77ef89bdde75e6c5e4a8ce5f8f3cf1359053ee4a
KimHyunSu <pn43@yanadoo.co.kr>
2021-04-19 13:47:51 +0900

    mac푸시테스트

--- frontend/src/component/contents/Faqcard.jsx
+++ frontend/src/component/contents/Faqcard.jsx
@@ -29,7 +29,7 @@ const Faqcard = () => {
       <section className="section-faq">
         <Title
           title="상위 FAQ: Jira Software Cloud가 나에게 도움이 될까요?"
-          desc="클라우드 개인 정보 보호, 보안, 안정성 및 규정 준수에 대한 Atlassian의 상위 FAQ를 확인하세요. 규정 준수 인증 및 세부 정보는 신뢰 사이트 를 참조하세요."
+          desc="클라우드 개인 정보 보호, 보안, 안정성 및 규정 준수에 대한 Atlassian의 상위 FAQ를 확인하세요. 규정 준수 인증 및 세부 정보는 신뢰 사이트 를 참조하세요. (맥 커밋 테스트)"
         />
         <ul>
           {faqList &&
